0d9c8f6ee967f559a2b0a8e089bbe0364320266a
Miguel Alvarez <kamapu78@gmail.com>
2020-11-21 11:02:41 +0100

    Optimized 'grasslands' data set

--- data-raw/grasslands.R
+++ data-raw/grasslands.R
@@ -1,7 +1,7 @@
-# TODO:   Add comment
+# TODO:   Import and arrangement of data set 'grasslands'
 # 
 # Author: Miguel Alvarez
-###############################################################################
+################################################################################
 
 library(vegtable)
 library(vegtable2)
@@ -13,6 +13,8 @@ load("grasslands.rda")
 conn <- connect_db2(dbname="veg_databases", user="miguel")
 
 grasslands <- import_sudamerica(conn, get_countries=FALSE)
+
+dbDisconnect(conn)
 
 # Subset database
 grasslands <- subset(grasslands, bibtexkey == "SanMartin1998", slot="relations",
@@ -30,9 +32,28 @@ grasslands@species@taxonTraits <-
 
 # Skip header information and add bibliographic information
 grasslands@header <- grasslands@header[!colnames(grasslands@header) %in%
-				c("validation_coordinates", "id_cldataveg")]
+				c("validation_coordinates", "id_cldataveg", "remarks")]
 
-# TODO: Add references
+# Re arrange slot samples
+grasslands@samples <- grasslands@samples[,c("record_id", "ReleveID",
+				"TaxonUsageID", "cover_percentage")]
+
+# Adding bibliographic references
+conn <- connect_db2(dbname="references_db", user="miguel")
+
+Query <- paste0("SELECT * FROM miguel2020.main_table\n",
+		"WHERE bibtexkey = 'SanMartin1998';")
+
+Ref <- dbGetQuery(conn, Query)
+
+dbDisconnect(conn)
+
+veg_relation(grasslands, "data_source") <- cbind(data_source=veg_relation(grasslands,
+				"data_source")$data_source,
+		Ref[, apply(Ref, 2, function(x) !is.na(x))])
+
+summary(grasslands)
+
 # Insert this script in the package source
 
 save(grasslands, file="../datastore/data/grasslands.rda")
