41b228a606d8de6efdca53a31f231ddd28b2c096
Miguel Alvarez <kamapu78@gmail.com>
2022-06-06 12:44:30 +0200

    Updated grasslands to new vegtable object structure

--- data-raw/grasslands.R
+++ data-raw/grasslands.R
@@ -3,24 +3,20 @@
 # Author: Miguel Alvarez
 ################################################################################
 
-library(vegtable)
-library(vegtable2)
-library(biblio)
-library(dbaccess)
+library(vegtableDB)
+library(biblioDB)
 
-load("grasslands.rda")
+conn <- connect_db(dbname = "vegetation_v3", user = "miguel")
 
-conn <- connect_db2(dbname="veg_databases", user="miguel")
-
-grasslands <- import_sudamerica(conn, get_countries=FALSE)
+grasslands <- db2vegtable(conn, database = "sudamerica")
 
 dbDisconnect(conn)
 
 # Subset database
-grasslands <- subset(grasslands, bibtexkey == "SanMartin1998", slot="relations",
-		relation="data_source")
-grasslands@species <- used_concepts(grasslands, keep_parents=TRUE)
-summary(grasslands)
+grasslands <- subset(grasslands, bibtexkey  ==  "SanMartin1998",
+    slot = "relations", relation = "data_source")
+grasslands@species <- used_concepts(grasslands, keep_parents = TRUE)
+grasslands
 
 # Change life forms
 grasslands@species@taxonTraits$life_form <-
@@ -28,7 +24,7 @@ grasslands@species@taxonTraits$life_form <-
 
 grasslands@species@taxonTraits <-
 		grasslands@species@taxonTraits[,
-				colnames(grasslands@species@taxonTraits) != "lf_elena"]
+				colnames(grasslands@species@taxonTraits) !=  "lf_elena"]
 
 # Skip header information and add bibliographic information
 grasslands@header <- grasslands@header[!colnames(grasslands@header) %in%
@@ -38,22 +34,5 @@ grasslands@header <- grasslands@header[!colnames(grasslands@header) %in%
 grasslands@samples <- grasslands@samples[,c("record_id", "ReleveID",
 				"TaxonUsageID", "cover_percentage")]
 
-# Adding bibliographic references
-conn <- connect_db2(dbname="references_db", user="miguel")
-
-Query <- paste0("SELECT * FROM miguel2020.main_table\n",
-		"WHERE bibtexkey = 'SanMartin1998';")
-
-Ref <- dbGetQuery(conn, Query)
-
-dbDisconnect(conn)
-
-veg_relation(grasslands, "data_source") <- cbind(data_source=veg_relation(grasslands,
-				"data_source")$data_source,
-		Ref[, apply(Ref, 2, function(x) !is.na(x))])
-
-summary(grasslands)
-
-# Insert this script in the package source
-
-save(grasslands, file="../datastore/data/grasslands.rda")
+save(grasslands, file = "data/grasslands.rda")
+## load("data/grasslands.rda")
